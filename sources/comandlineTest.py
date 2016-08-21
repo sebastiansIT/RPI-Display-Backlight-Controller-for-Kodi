c161c6ef2f352b802c3aa7604fabf6c99cfbaa60
SebastiansIT <sebastian@human-injection.de>
2016-08-21 17:19:44 +0200

    change one line test script to simple command line client

--- sources/comandlineTest.py
+++ sources/comandlineTest.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python
 
 import os
-# import subprocess
+import argparse
 
-# brightnessDevice = open('/sys/devices/platform/rpi_backlight/backlight/rpi_backlight/brightness', 'w')
-# subprocess.call(["echo", "150"], stdout=brightnessDevice)
+def disableBacklight():
+    os.system('echo 1 > /sys/class/backlight/rpi_backlight/bl_power')
+    
+def enableBacklight():
+    os.system('echo 0 > /sys/class/backlight/rpi_backlight/bl_power')
+    
+def setBrightness( brightness ):
+    os.system('echo ' + str(brightness) + ' > /sys/class/backlight/rpi_backlight/brightness')
+    
+parser = argparse.ArgumentParser(description='Manipulate the Backlight of the Offical Raspberry Pi 7" Touch Display.')
+parser.add_argument('-b', '--brightness', dest='brightness', nargs='?', default=255, type=int, choices=range(0, 256), help='Set the brightness of the display betwean 0 and 255')
+parser.add_argument('-p', '--power', dest='power', nargs='?', default=1, type=int, choices=[0,1], help='Activate (1) or deactivate (0) the backlight')
 
-os.system('echo 50 > /sys/class/backlight/rpi_backlight/brightness')
-
+args = parser.parse_args()
+print args.power
+print args.brightness
+if args.power == 0:
+    disableBacklight()
+else:
+    enableBacklight()
+setBrightness(args.brightness)
